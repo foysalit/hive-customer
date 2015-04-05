7e1011a59caaa4a6b357c7acff7c0fbf5f0867a6
Foysal Ahamed <cfaion341@gmail.com>
2015-04-05 13:41:09 +0200

    consumer crud is setup

---
+++ app/models/consumer.server.model.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+var validateProperty = function(property) {
+	return (!this.updated || property.length);
+};
+
+/**
+ * Consumer Schema
+ */
+var ConsumerSchema = new Schema({
+	firstName: {
+		type: String,
+		trim: true,
+		default: '',
+		validate: [validateProperty, 'Please fill in consumer\'s first name']
+	},
+	lastName: {
+		type: String,
+		trim: true,
+		default: '',
+		validate: [validateProperty, 'Please fill in consumer\'s last name']
+	},
+	address: {
+		type: String,
+		trim: true,
+		default: '',
+		validate: [validateProperty, 'Please fill in consumer\'s address']
+	},
+	phone: {
+		type: String,
+		trim: true,
+		default: '',
+		validate: [validateProperty, 'Please fill in your phone number']
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+
+mongoose.model('Consumer', ConsumerSchema);
